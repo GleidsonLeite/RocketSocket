9a8ba9fb3a9c6a2ec5504a6b2c7b47df983c044f
Gleidson Silva <gleidsonsilva@pop-os.localdomain>
2022-10-19 23:05:10 -0300

    Socket.io configured

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,6 @@
-import express, { Request, Response } from 'express'
+import { server } from "./http";
+import "./websocket/ChatService"
 
-const app = express();
-
-app.get("/", (request:Request, response:Response) => {
-  return response.json({
-    message: "Hello Websocket"
-  })
-})
-
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log("Server is running on port 3000");
 })
